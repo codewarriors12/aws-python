dadc7f7dfa5329e7b5ded9b13fe24e17a2a4faff
codewarriors12 <pdimple12@gmail.com>
2023-02-22 01:41:28 +0000

    new python file commited - ListFiles.py

---
+++ ListFiles.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+#The OS module  built-in library within Python makes available different functions that are used to communicate with the operating system. 
+# This module is part of Python’s standard utility modules.
+
+#os module to collect information about the files in our current working directory.
+# The os.getcwd() method is one of the functions which the OS module provides.
+import os
+
+# get current working dir (same as pwd in linux but) in python it is function getcwd() of OS module
+mydir = str(input("enter the directory full path: "))
+#print (mydir)
+
+if mydir == '':
+    print ("you have not entered dir path, thus we will take current dir as selected.")
+    mydir = os.getcwd()
+    print ("my dir is : ",mydir)
+
+else:
+    print ("you have selected directory : ",mydir)
+    #sys.exit()
+
+# Create an empty list which we will later be using to add file related info
+list_files = []
+
+# os.scandir() function – a better and faster directory iterator/current working dir
+for file in os.scandir(mydir):
+        
+# Get the file path, file name, and size
+        file_path = str(file.path)
+        file_name = str(file.name)
+        file_size = str(file.stat().st_size)
+
+# file info is a veriable which will have the file information
+        file_details = {"file name is - "+ file_name + " , file size is - "+ file_size + " bytes, and file path is - "+file_path}
+                                    
+# Append/add list of files in to the empty dictionary
+        list_files.extend(file_details)
+                                                
+# Print the list of files, printed onto separate lines
+print ("you have total : " + str(len(list_files)) + " files.")
+print(*list_files, sep="\n")
