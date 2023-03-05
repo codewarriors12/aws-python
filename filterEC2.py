a1fda1232222f4254121882198a148e3c96c4223
codewarriors12 <pdimple12@gmail.com>
2023-03-05 05:03:49 +0000

    automate EC2 to stop

---
+++ filterEC2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import boto3
+ec2=boto3.resource('ec2')
+state = 'running'
+instance = ec2.instances.filter(Filters=[{
+    'Name': 'instance-state-name',
+    'Values': ['state']}])
+print (f' instances are "{state}":')
+for instance in instance:
+    print(f' instance id: {instance.id}')
